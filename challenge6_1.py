fbfa6de1a55c71c9360a18cf7179daae0efcf6f7
mzg <15122302865@163.com>
2018-04-25 21:29:43 +0800

    first commite

---
+++ challenge6_1.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+import pandas as pd
+def co2(dataset):
+    # 读取世界银行气候变化数据集
+    df_climate = pd.read_excel('ClimateChange.xlsx',sheetname='Data')
+    df_country = pd.read_excel('ClimateChange.xlsx',sheetname='Country')
+    df_series = pd.read_excel('ClimateChange.xlsx',sheetname='Series')
+    # 1.查看数据文件结构
+    
+    # 2.将国家和所在的收入群体类别产生联系
+
+    # 3.处理 DataFrame 中的不必要数据和缺失数据
+    # 4.注意缺失值并不是NaN的形式
+    # 5.将最终返回的 DataFrame 处理成挑战要求的格式
+
+    
+    return results
